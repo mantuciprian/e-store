b0caff3e876b2302d73d3fd60d8b8e2fbbedce0d
Ciprian Mantu <c_cmantu@groupon.com>
2019-02-28 16:50:42 +0200

    admin app added and server connection set

--- server/dist/DB/connector/connector.js
+++ server/dist/DB/connector/connector.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Pool = require("pg");
+const pg_1 = require("pg");
 //connect to the DB
-const pool = new Pool({
+const pool = new pg_1.Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'VK_mail',
-    password: 'ciprian',
+    database: 'e-store',
+    password: '12345',
     port: 5432,
 });
 module.exports = pool;
--- server/dist/routes/home.js
+++ server/dist/routes/home.js
@@ -1,11 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const pool = require("../DB/connector/connector");
 class Home {
     constructor() {
         this.router = express.Router();
+        this.pool = pool;
         this.router.get('/', (req, res) => {
             res.send('home page');
+        });
+        this.router.get('/test', (req, res) => {
+            const sender = res;
+            this.pool.query('SELECT * FROM users', (err, res) => {
+                if (err) {
+                    console.log(err);
+                    sender.send(500);
+                    return;
+                }
+                else {
+                    console.log(res.rows);
+                    sender.send(res.rows);
+                }
+            });
         });
     }
     ;
